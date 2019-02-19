bd0bb5247c24c55caf17c90bd761948fb8243d6e
Nurikhsan <ikhsanhikari29@gmail.com>
2019-02-19 10:26:22 +0700

    init

---
+++ oauth-sample/src/main/java/com/hikari/oauth/oauthsample/config/KonfigurasiSecurity.java
@@ -0,0 +1,31 @@
+package com.hikari.oauth.oauthsample.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("hikari").password("hikari29").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+        .defaultSuccessUrl("/hikari").
+        and().logout();
+    }
+}
